5875b978beacb6e879d1bb507a85db27ab848060
Maxi Rodriguez <maxirodriguez573@gmail.com>
2024-05-16 22:42:16 -0300

    Creación del menú

--- Proyecto.py
+++ Proyecto.py
@@ -1,3 +1,15 @@
 # Import's
 import random
 import matplotlib.pyplot as plt
+
+# Menú
+def menu():
+    print('''
+1 - Jugar Piedra, papel, tijera
+        
+2 - Adivinar el número contra la computadora
+    
+3 - Tirar un dado
+        
+4 - Graficar una función matemática
+    ''')
